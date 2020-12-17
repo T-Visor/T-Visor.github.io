d3c1853e9273d1b7f31f8e866d7d2fea4a94cf5d
T-Visor <turkimbro@yahoo.com>
2020-12-17 17:17:48 -0500

    modified contents to work with current site

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'info@fruitsandveggiesfun.com>';
+$from = 'info@t-visor.github.io/>';
 
 // an email address that will receive the email with the output of the form
 $sendTo = 'turkimbro@gmail.com';
